818d9d98eacf97baa64d513d86cfa4d1b225e7ec
Amir <nik.amirsa@gmail.com>
2020-05-16 19:48:13 +0430

    chat card - p1

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,23 +1,45 @@
 import React from 'react';
-import { Button, Row, Col, Avatar, Card, Layout } from 'antd';
+import { Button, Row, Col, Avatar, Card, Layout, Input, Tooltip } from 'antd';
+import { SmileOutlined } from '@ant-design/icons';
 
 const Chat = () => {
   return (
     <div className='container'>
-      <Layout style={{ padding: '0 50px' }}>
+      <Layout style={{ padding: '0 50px', background: 'transparent' }}>
         <Row>
           <Col span={12}>
-            <Card title='Card title' bordered={false} style={{ width: 300 }}>
-              <p>Card content</p>
-              <p>Card content</p>
-              <p>Card content</p>
+            <Card
+              title='Amir'
+              extra={
+                <Avatar
+                  style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}
+                >
+                  U
+                </Avatar>
+              }
+              bordered={false}
+              style={{ width: 350 }}
+            >
+              <div className='messages'>
+                <p>text</p>
+
+                <p>text</p>
+              </div>
+
+              <div className='input-chat'>
+                <Row>
+                  <Col flex={2} style={{ textAlign: 'left' }}>
+                    <Tooltip title='Emoji'>
+                      <Button shape='circle' icon={<SmileOutlined />} />
+                    </Tooltip>
+                  </Col>
+
+                  <Col flex={10}>
+                    <Input placeholder='Type a message...' />
+                  </Col>
+                </Row>
+              </div>
             </Card>
-
-            <Avatar style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}>
-              U
-            </Avatar>
-
-            <Button type='primary'>Button 1</Button>
           </Col>
           <Col span={12}>
             <Button type='danger'>Button 2</Button>
