ad9bf03a37dacfee16ddf9c43ae4de7d9094a130
Amir <nik.amirsa@gmail.com>
2020-05-20 07:37:37 +0430

    add checkboxes to modal

--- src/components/ModalSetting.js
+++ src/components/ModalSetting.js
@@ -1,7 +1,23 @@
 import React from 'react';
-import { Modal, Input } from 'antd';
+import { Modal, Input, Checkbox } from 'antd';
 
 const ModalSetting = () => {
+  function onChange(checkedValues) {
+    console.log('checked = ', checkedValues);
+  }
+
+  const plainOptions = ['Apple', 'Pear', 'Orange'];
+  const options = [
+    { label: 'Apple', value: 'Apple' },
+    { label: 'Pear', value: 'Pear' },
+    { label: 'Orange', value: 'Orange' },
+  ];
+  const optionsWithDisabled = [
+    { label: 'Apple', value: 'Apple' },
+    { label: 'Pear', value: 'Pear' },
+    { label: 'Orange', value: 'Orange', disabled: false },
+  ];
+
   return (
     <div>
       <Modal title='Setting' visible={true}>
@@ -11,7 +27,22 @@ const ModalSetting = () => {
             <Input placeholder='Name of user' />
           </label>
         </p>
-        {/* <p>Some contents...</p> */}
+        <p>
+          <div>
+            <Checkbox.Group
+              options={plainOptions}
+              defaultValue={['Apple']}
+              onChange={onChange}
+            />
+            <br />
+            <br />
+            <Checkbox.Group
+              options={options}
+              defaultValue={['Pear']}
+              onChange={onChange}
+            />
+          </div>
+        </p>
       </Modal>
     </div>
   );
