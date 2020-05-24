60c92314d9b7074a31f8c934a628790cc57318c2
Amir <nik.amirsa@gmail.com>
2020-05-24 09:45:08 +0430

    add Vazir font for Arabic and Persian

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Chat from './pages/Chat';
 import MainState from './context/MainState';
+import 'vazir-font/dist/font-face.css';
 import './App.css';
 
 function App() {
