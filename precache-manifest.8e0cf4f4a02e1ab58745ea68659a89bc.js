bbd563fe9be72f4ef10662817768059c2e026e42
Amir <nik.amirsa@gmail.com>
2021-03-05 17:48:47 +0330

    Updates

---
+++ precache-manifest.8e0cf4f4a02e1ab58745ea68659a89bc.js
@@ -0,0 +1,270 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b9aeba2810a5b89f531b98ba04273a",
+    "url": "/react-simulation-chat/index.html"
+  },
+  {
+    "revision": "27430f46ebce8e101e73",
+    "url": "/react-simulation-chat/static/css/2.fe56744f.chunk.css"
+  },
+  {
+    "revision": "a01cab6b7b907450d7cf",
+    "url": "/react-simulation-chat/static/css/main.71d4ecd6.chunk.css"
+  },
+  {
+    "revision": "27430f46ebce8e101e73",
+    "url": "/react-simulation-chat/static/js/2.ca9573fc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-simulation-chat/static/js/2.ca9573fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a01cab6b7b907450d7cf",
+    "url": "/react-simulation-chat/static/js/main.383be43c.chunk.js"
+  },
+  {
+    "revision": "7617e1f2c6f583010985",
+    "url": "/react-simulation-chat/static/js/runtime-main.37ac8f95.js"
+  },
+  {
+    "revision": "059a404e6552c6e425acf8d4d1ebb42b",
+    "url": "/react-simulation-chat/static/media/Vazir-Black.059a404e.woff"
+  },
+  {
+    "revision": "58b9fd180f105c5fb4b07581e3c38eaa",
+    "url": "/react-simulation-chat/static/media/Vazir-Black.58b9fd18.woff2"
+  },
+  {
+    "revision": "d13e84132d50f0661fb54741c0c3209b",
+    "url": "/react-simulation-chat/static/media/Vazir-Black.d13e8413.eot"
+  },
+  {
+    "revision": "dfa43d456d988b30955a0476ba003db0",
+    "url": "/react-simulation-chat/static/media/Vazir-Black.dfa43d45.ttf"
+  },
+  {
+    "revision": "2c7bb6b6d21da6d739cd10d346d0a3d3",
+    "url": "/react-simulation-chat/static/media/Vazir-Bold.2c7bb6b6.woff2"
+  },
+  {
+    "revision": "86e9c7443a7cbebd5eae1b07b85788cf",
+    "url": "/react-simulation-chat/static/media/Vazir-Bold.86e9c744.eot"
+  },
+  {
+    "revision": "b0205da67c5b08ef3f04b38c44cff525",
+    "url": "/react-simulation-chat/static/media/Vazir-Bold.b0205da6.woff"
+  },
+  {
+    "revision": "eda9cf8500fbd5dc55210ab85f7c9f97",
+    "url": "/react-simulation-chat/static/media/Vazir-Bold.eda9cf85.ttf"
+  },
+  {
+    "revision": "5d36c804f96cecf94aaef7b88f6a86ac",
+    "url": "/react-simulation-chat/static/media/Vazir-Light.5d36c804.woff"
+  },
+  {
+    "revision": "ace04414b51d16478a8f688f40a5f0be",
+    "url": "/react-simulation-chat/static/media/Vazir-Light.ace04414.ttf"
+  },
+  {
+    "revision": "e8c4a74f717269c78953092bb32bf7bd",
+    "url": "/react-simulation-chat/static/media/Vazir-Light.e8c4a74f.woff2"
+  },
+  {
+    "revision": "fd376c5bf99adfe2174b29b95b71e785",
+    "url": "/react-simulation-chat/static/media/Vazir-Light.fd376c5b.eot"
+  },
+  {
+    "revision": "38f9fc2b12ffe3f7634e68478b18fdf9",
+    "url": "/react-simulation-chat/static/media/Vazir-Medium.38f9fc2b.woff2"
+  },
+  {
+    "revision": "4422f8b558f778c6f97b36c6c333ca58",
+    "url": "/react-simulation-chat/static/media/Vazir-Medium.4422f8b5.ttf"
+  },
+  {
+    "revision": "955e4ed51215d39725409a5f79b3e8aa",
+    "url": "/react-simulation-chat/static/media/Vazir-Medium.955e4ed5.woff"
+  },
+  {
+    "revision": "d68f6eb098111785bc068ff8dd7ecfc1",
+    "url": "/react-simulation-chat/static/media/Vazir-Medium.d68f6eb0.eot"
+  },
+  {
+    "revision": "b7ce981cc62af9f3fe55d1e488d35d91",
+    "url": "/react-simulation-chat/static/media/Vazir-Thin.b7ce981c.ttf"
+  },
+  {
+    "revision": "db050bddbc6445b6bb6486e4bb1c042d",
+    "url": "/react-simulation-chat/static/media/Vazir-Thin.db050bdd.eot"
+  },
+  {
+    "revision": "de353b2a4b6d7e195bed9df133cf068f",
+    "url": "/react-simulation-chat/static/media/Vazir-Thin.de353b2a.woff"
+  },
+  {
+    "revision": "f7c409716d0d750dc8f533e87a729feb",
+    "url": "/react-simulation-chat/static/media/Vazir-Thin.f7c40971.woff2"
+  },
+  {
+    "revision": "a91231a66c2be0dfc22723c9347cde98",
+    "url": "/react-simulation-chat/static/media/Vazir.a91231a6.woff2"
+  },
+  {
+    "revision": "abdaaf2e791f4416323efffec0a85b49",
+    "url": "/react-simulation-chat/static/media/Vazir.abdaaf2e.woff"
+  },
+  {
+    "revision": "f5ee3f2f7422a0d0ef67d7c5bc7742d9",
+    "url": "/react-simulation-chat/static/media/Vazir.f5ee3f2f.ttf"
+  },
+  {
+    "revision": "fd58729f3709a9e093f9692c9e4914bf",
+    "url": "/react-simulation-chat/static/media/Vazir.fd58729f.eot"
+  },
+  {
+    "revision": "191cc9f50f3b76b9617cb383f19acb7d",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-100.191cc9f5.woff2"
+  },
+  {
+    "revision": "370318464551d5f25b0f0a78f374faac",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-100.37031846.woff"
+  },
+  {
+    "revision": "bdeaeb79db315697bd173a55b097dc18",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-100italic.bdeaeb79.woff2"
+  },
+  {
+    "revision": "ecf7d49386e8f265878e735db34a7c4b",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-100italic.ecf7d493.woff"
+  },
+  {
+    "revision": "1fc98e126a3d152549240e6244d7e669",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-200.1fc98e12.woff"
+  },
+  {
+    "revision": "85d5ef9db7f2dc6979172a4a3b2c57cb",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-200.85d5ef9d.woff2"
+  },
+  {
+    "revision": "49095760a498d024fe1a85a078850df9",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-200italic.49095760.woff2"
+  },
+  {
+    "revision": "fe46cf8b9462c820457d3bf537e4057f",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-200italic.fe46cf8b.woff"
+  },
+  {
+    "revision": "7c3daf12b706645b5d3710f863a4da04",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-300.7c3daf12.woff2"
+  },
+  {
+    "revision": "8dc95fab9cf98d02ca8d76e97d3dff60",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-300.8dc95fab.woff"
+  },
+  {
+    "revision": "3fe16939288856e8e828fa2661bf2354",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-300italic.3fe16939.woff"
+  },
+  {
+    "revision": "f20b178ca2024a5eac8e42e6649db86c",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-300italic.f20b178c.woff2"
+  },
+  {
+    "revision": "8102c4838f9e3d08dad644290a9cb701",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-400.8102c483.woff"
+  },
+  {
+    "revision": "bc3aa95dca08f5fee5291e34959c27bc",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-400.bc3aa95d.woff2"
+  },
+  {
+    "revision": "5cad650422a7184467af5a4d17b264c4",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-400italic.5cad6504.woff2"
+  },
+  {
+    "revision": "d191f22af3bb50902b99ac577f81a322",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-400italic.d191f22a.woff"
+  },
+  {
+    "revision": "8b763220218ffc11c57c84ddb80e7b26",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-500.8b763220.woff"
+  },
+  {
+    "revision": "92d16e458625f4d2c8940f6bdca0ff09",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-500.92d16e45.woff2"
+  },
+  {
+    "revision": "47bfcca6b69d6a9acca7a8bff17193e2",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-500italic.47bfcca6.woff2"
+  },
+  {
+    "revision": "72c01f753c3940c0b9cb6bf2389caddf",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-500italic.72c01f75.woff"
+  },
+  {
+    "revision": "6fb1b5623e528e27c18658fecf5ee0ee",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-600.6fb1b562.woff2"
+  },
+  {
+    "revision": "7c839d15a6f54e7025ba8c0c4b333e8f",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-600.7c839d15.woff"
+  },
+  {
+    "revision": "60789af1c9338ed1a9546722ec54b4f7",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-600italic.60789af1.woff2"
+  },
+  {
+    "revision": "f3d4de8d0afb19e777c79032ce828e3d",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-600italic.f3d4de8d.woff"
+  },
+  {
+    "revision": "39d93cf678c740f9f6b2b1cfde34bee3",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-700.39d93cf6.woff2"
+  },
+  {
+    "revision": "80f10bd382f0df1cd650fec59f3c9394",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-700.80f10bd3.woff"
+  },
+  {
+    "revision": "8c98142b425630821139c24bd1698700",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-700italic.8c98142b.woff"
+  },
+  {
+    "revision": "ba136d97b14e82284dd595e257f11c47",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-700italic.ba136d97.woff2"
+  },
+  {
+    "revision": "9a9befcf50d64f9d2d19d8b1d1984add",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-800.9a9befcf.woff"
+  },
+  {
+    "revision": "b7018be9ed6cd94da8b6675b3a468c3b",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-800.b7018be9.woff2"
+  },
+  {
+    "revision": "540ffdd223d1a9ad3d4e678e1a23372e",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-800italic.540ffdd2.woff2"
+  },
+  {
+    "revision": "897086f99f4e1f45e6b1e9368527d0bc",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-800italic.897086f9.woff"
+  },
+  {
+    "revision": "26d42c9428780e545a540bbb50c84bce",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-900.26d42c94.woff"
+  },
+  {
+    "revision": "58cd789700850375b834e8b6776002eb",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-900.58cd7897.woff2"
+  },
+  {
+    "revision": "451157bc8861fe54f523b3669a3def71",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-900italic.451157bc.woff2"
+  },
+  {
+    "revision": "a8ec4957e1c24f5793305763ad9845b3",
+    "url": "/react-simulation-chat/static/media/montserrat-latin-900italic.a8ec4957.woff"
+  }
+]);
